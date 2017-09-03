c049ca373ec4281d8990bbaa4ffac2d695749015
mirman-mtaggart <mtaggart@mirman.org>
2017-09-02 20:16:38 -0700

    2 start tests

---
+++ 2-compass/index.js
@@ -0,0 +1,10 @@
+function compass(heading) {
+    // We're going to convert a compass heading (0 - 359) to a named direction
+    // Compass directions are: 
+    // N, NNE, ENE, E, ESE, SSE, E, S, SSW, WSW, W, WNW, NNW
+    return null;
+}
+
+
+// We need this line to run tests
+module.exports = compass;
